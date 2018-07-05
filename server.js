3cd2fdd3ed690367ebd0bf7fcd2d5adbbf1757b2
Richard Deane <titchimoto@gmail.com>
2018-07-04 22:37:56 -0700

    Heroku postbuild setup

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const users = require("./routes/api/users");
 const library = require("./routes/api/library");
@@ -20,9 +21,15 @@ mongoose
 app.use(passport.initialize());
 require("./config/passport")(passport);
 
-app.use('/', express.static('./client/public'));
 app.use("/api/users", users);
 app.use("/api/library", library);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('./client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirName, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
